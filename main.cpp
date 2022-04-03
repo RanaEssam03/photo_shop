ff965842887ea62c9f211ec6ec1888857717c35b
Rana Essam <95047435+RanaEssam03@users.noreply.github.com>
2022-04-03 21:21:10 +0200

    add filter 7

--- main.cpp
+++ main.cpp
@@ -10,14 +10,15 @@ void loadImage ();
 void saveImage ();
 void bAndW ();
 void flipImage();
+void detectImageEdges();
 
 int main() {
     cout << "Ahlan ya user ya habibi " << char(1) << endl;
+    char filter;
+    cout <<"Please enter file name of the image to process : " << endl << "->";
+    loadImage();
     while (true){
-        char filter;
-       cout <<"Please enter file name of the image to process : " << endl << "->";
-        loadImage();
-       cout << "Please select a filter to apply or 0 to exit:\n";
+        cout << "Please select a filter to apply or 0 to exit:\n";
        cout << "1-Black & white Filter\n2-Invert Filter\n3-Merge Filter\n4-Flip Image\n5-Darken and Lighten Image";
        cout << "\n6- Rotate Image\n7- Detect Image Edges\n8- Enlarge Image\n9- Shrink Image\na- Mirror 1/2 Image";
        cout << "\nb- Shuffle Image\nc- Blur Image\ns- Save the image to a file\n0- Exit\n-->";
@@ -33,13 +34,15 @@ int main() {
         else if (filter == '4'){
             flipImage();
         }
-        else if (filter == '5'){
-            continue;
+        else if (filter == '7'){
+           detectImageEdges();
         }
-
-        saveImage();
-
+        else if (filter == 's'){
+            saveImage();
+            break;
+        }
     }
+
     return 0;
 }
 //_________________________________________________
@@ -103,6 +106,22 @@ void flipImage(){
                 image[i][j] = image[i][SIZE-j-1];
                 image[i][SIZE-j-1] = temp;
             }
+        }
+    }
+}
+
+
+void detectImageEdges(){
+    bAndW();
+    for (int i = 0 ; i <255; i++){
+        for (int j = 0 ; j < 255; j++){
+            if (image[i][j] != image[i][j+1]){
+                image[i][j] =0;
+            }
+            else if (image[i][j] == image[i][j+1]){
+                image[i][j] = 225;
+            }
+
         }
     }
 }
